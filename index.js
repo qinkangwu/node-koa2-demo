844168764bf5f475493a0a57166f5c13db74b444
admin <qinkangwu@xs1h.com>
2018-04-08 11:39:57 +0800

    asd

--- index.js
+++ index.js
@@ -10,22 +10,28 @@ const client = new Eureka({
       app: 'qkw_test',
       hostName: 'localhost',
       ipAddr: '127.0.0.1',
-      port: 3000,
+      port: {
+        '$': 3000,
+        '@enabled': true,
+      },
       vipAddress: 'test.something.com',
+      statusPageUrl : 'http://localhost:3000/api-front/info',
       dataCenterInfo: {
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
         name: 'MyOwn',
       },
     },
     eureka: {
       // eureka server host / port
-      host: '192.168.99.100',
-      port: 32768,
+      host: '172.16.1.97',
+      port: 8761,
+      servicePath : '/eureka'
     },
 });
 
-// client.start((err)=>{
-//     console.log(err || 'node app erueka register completed ~');
-// })
+client.start((err)=>{
+    console.log(err || 'node app erueka register completed ~');
+})
 
 app.use(logMiddleware());
 app.use(bodyParser())
--- router/index.js
+++ router/index.js
@@ -1,6 +1,8 @@
 const api = require('koa-router')();
 const user = require('./user');
+const info = require('./info');
 
 api.use(user.routes());
+api.use(info.routes());
 
 module.exports = api.use('/api-front',api.routes(),api.allowedMethods());
